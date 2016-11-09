7f948e8875d9f0f80018d1edb1a0be809b78b284
Anton Kramarov <kas_2002@mail.ru>
2016-11-09 14:00:47 +0200

    KZ3.1 - 2

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ewgu02a*ev&c_7wcoh6ouq#x29+)i*0)@huc#^e9gnz=u*%@y#'
+SECRET_KEY = 'x_5mw=l15(24ls&vv+j_j*z_u%wvn6syezj)hx=a4scm0%s1wl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
