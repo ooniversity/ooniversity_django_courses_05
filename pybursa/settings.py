da350887845f318f37076d7b1a6eab266574b8f1
toldrew <tol_drew@mail.ru>
2016-11-11 16:10:10 +0700

    K_Z_3_2

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -55,7 +55,8 @@ ROOT_URLCONF = 'pybursa.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,5 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 STATIC_URL = '/static/'
--- pybursa/urls.py
+++ pybursa/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 
 from django.conf.urls import include, url
 from django.contrib import admin
+from pybursa.views import index, contact, student_detail, student_list
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^$', index, name='index'),
+    url(r'^contact/$', contact, name='contact'),
+    url(r'^student_list/$', student_list, name='student_list'),
+    url(r'^student_detail/$', student_detail, name='student_detail'),
 ]
