27c43d758be3ef946f584a67421b3038e5a372f1
Lyudmyla Kaluzhynova <kaluzhynova@gmail.com>
2016-11-11 11:55:00 +0200

    added kr3

---
+++ pybursa/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import render
+
+def index(request):
+    return render(request, "index.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def student_list(request):
+    return render(request, "student_list.html")
+
+def student_detail(request):
+    return render(request, "student_detail.html")
+
+
+# Create your views here.
