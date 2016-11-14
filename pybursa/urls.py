aec9f6e2c9d4c95c24f7cf7802f1996fed7c6d5e
vento357 <vento357@gmail.com>
2016-11-15 00:09:39 +0200

    render

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -15,14 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from . import views
+from pybursa import views
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index, name='index'),
-    url(r'^index/$', views.index, name='index'),
-    url(r'^contact/$', views.contact, name='contact'),
-    url(r'^student_list/$', views.student_list, name='student_list'),
-    url(r'^student_detail/$', views.student_detail, name='student_detail')
+    url(r'^$', views.index, name = 'index'),
+    url(r'^contact/', views.student_detail, name='student_detail'),
+    url(r'^$', views.student_list, name = 'student_list'),
+    url(r'^contact/', views.contact, name='contact'),
 ]
