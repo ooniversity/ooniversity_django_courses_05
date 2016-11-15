3a2470844695fb333873a5697c1f4552c070fac8
Your Name <you@example.com>
2016-11-16 00:50:42 +0200

    KR3_2

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from .views import index, contact, student_list, student_detal
+from .views import index, contact, student_list, student_detail
+
 
 
 urlpatterns = [
     url(r'^$', index, name='index'),
-    url(r'^contact/',  contact, name='contact'),
+    url(r'^index/$', index, name='index'),
+    url(r'^contact/$',  contact, name='contact'),
     url(r'^student_list/$', student_list, name='student_list'),
     url(r'^student_detail/$', student_detail, name='student_detail'),
+    url(r'^polls/', include('polls.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
