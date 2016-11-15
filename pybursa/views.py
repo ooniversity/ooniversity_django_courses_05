c446b0b447ee61e0cd3b7c84230cc57d71b7d82c
denis fursov <denis.fursov@outlook.com>
2016-11-16 00:23:54 +0200

    KR3_2

--- pybursa/views.py
+++ pybursa/views.py
@@ -1,14 +1,20 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
 
 def index(request):
-     return render(request, "index.html")
+    template = loader.get_template('templates/index.html')
+    return HttpResponse(template.render(request))
 
 def contact(request):
-     return render(request, "contact.html")
+    template = loader.get_template('templates/contact.html')
+    return HttpResponse(template.render(request))
 
 def student_list(request):
-     return render(request, "student_list.html")
+    template = loader.get_template('templates/student_list.html')
+    return HttpResponse(template.render(request))
 
 def student_detail(request):
-return render(request, "student_detail.html")
+    template = loader.get_template('templates/student_detail.html')
+    return HttpResponse(template.render(request))
