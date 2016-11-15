f06eef56dbe795a6d12f5a94a9da9f67de10ee60
eugalata <eugalata@ubuntu-16.04>
2016-11-15 23:47:33 +0200

    v11

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -55,7 +55,10 @@ ROOT_URLCONF = 'pybursa.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': 
+        [
+            'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
