6b3199bd936111094a1824adf062acccfd9bf46b
toldrew <tol_drew@mail.ru>
2016-11-15 15:49:07 +0700

    fix bug tests 3-6

--- quadratic/views.py
+++ quadratic/views.py
@@ -4,26 +4,26 @@ from django.http import HttpResponse
 def quadratic_results(request):
     try:
         a = int(request.GET['a'])
-        if a == 0: a = '0 коэффициент при первом слагаемом уравнения не может быть равень нулю'
+        if a == 0: a = '0<br/>коэффициент при первом слагаемом уравнения не может быть равным нулю'
 
     except:
-        if request.GET['a'] == '': a = ' коэффициент не опеределен'
-        else: a = request.GET['a'] + ' коэффициент не целое число'
+        if request.GET['a'] == '': a = '<br/>коэффициент не определен'
+        else: a = request.GET['a'] + '<br/>коэффициент не целое число'
     try:
         b = int(request.GET['b'])
     except:
-        if request.GET['b'] == '': b = 'коэффициент не опеределен'
-        else: b = request.GET['b'] + ' коэффициент не целое число'
+        if request.GET['b'] == '': b = '<br/>коэффициент не определен'
+        else: b = request.GET['b'] + '<br/>коэффициент не целое число'
     try:
         c = int(request.GET['c'])
     except:
-        if request.GET['c'] == '': c = 'коэффициент не опеределен'
-        else: c = request.GET['c'] + ' коэффициент не целое число'
+        if request.GET['c'] == '': c = '<br/>коэффициент не определен'
+        else: c = request.GET['c'] + '<br/>коэффициент не целое число'
     if isinstance(a,int) and a != 0 and isinstance(b,int) and isinstance(c,int):
         d = b*b - 4*a*c
         if d > 0:
-            x1 = (-b + d**(1/2))/ (2*a)
-            x2 = (-b - d**(1/2)) / (2 * a)
+            x1 = round((-b + d**(1/2))/ (2*a),1)
+            x2 = round((-b - d**(1/2)) / (2 * a),1)
             k = {'a': a, 'b': b, 'c': c, 'd': 'Дискриминант: '+str(d), 'x': 'Квадратное уравнение имеет два действительных корня: x1 = '+str(x1) +', x2 = '+str(x2) }
         if d == 0:
             x1 = (-b) / (2 * a)
