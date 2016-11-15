d72a6f3c3881b49dd73ced51cd62971b7736be21
denis fursov <denis.fursov@outlook.com>
2016-11-15 23:20:14 +0200

    KR3_2

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from .views import index, contact, student_list, student_detail
+from pybursa import index, contact, student_list, student_detail
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
