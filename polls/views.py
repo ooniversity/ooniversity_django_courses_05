edc3d41f3a807f4120ab3932dcff5468d405673a
kazantip <kazzantipoff@gmail.com>
2016-11-15 11:47:25 +0200

    Redacted 15.11.2016_11.45AM

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.views import generic
-
 from .models import Choice, Question
 
 
