889477f001f757cdf0bd2be6c7309e11bed43e55
denis fursov <denis.fursov@outlook.com>
2016-11-15 21:45:55 +0200

    KR3_1_t4_new

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
-
 from .models import Choice, Question
 
 class IndexView(generic.ListView):
