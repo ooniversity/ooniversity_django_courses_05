8e4cf8a92023a480bddb62b6dba9f55a2429a2cd
TatyanaPaschenko <vivid.tanya@gmail.com>
2016-11-16 17:07:07 +0200

    kr4

--- quadratic/urls.py
+++ quadratic/urls.py
@@ -4,6 +4,7 @@ from .views import quadratic_results
 
 app_name = 'quadratic'
 urlpatterns = [
-    url(r'^results/(?P<a>[0-9])&(?P<b>[0-9])&(?P<c>[0-9])/$', quadratic_results, name = 'results'),
+    # url(r'^results/(?P<a>[0-9])&(?P<b>[0-9])&(?P<c>[0-9])/$', quadratic_results, name = 'results'),
+    url(r'^results/$', quadratic_results, name='results'),
 
 ]
--- quadratic/views.py
+++ quadratic/views.py
@@ -1,43 +1,21 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
-def quadratic_results(request, a, b, c):
+def quadratic_results(request):
 
-    try:
-        int_a = int(a)
-    except ValueError:
-        message_int_a = 'коэффициент не целое число'
-        message_int = 'коэффициент не целое число'
+    a = request.GET.get('a')
+    b = request.GET.get('b')
+    c = request.GET.get('c')
+    x1 = 0
+    x2 = 0
 
-    try:
-        int_b = int(b)
-    except ValueError:
-        message_int_b = 'коэффициент не целое число'
-        message_int = 'коэффициент не целое число'
-
-    try:
-        int_c = int(c)
-    except ValueError:
-        message_int_c = 'коэффициент не целое число'
-        message_int = 'коэффициент не целое число'
-
-    if a == '' or b == '' or c == '':
-        message_not_arg = 'коэффициент не определен'
-
-    if a == '0':
-        message_null = 'коэффициент при первом слагаемом уравнения не может быть равным нулю'
-
-    if message_int == '' and message_not_arg == '' and message_null == '':
-        descr = int_b ** 2 - 4 * int_a * int_c
-        if descr > 0:
-            x1 = (-int_b + descr ** (1/2)) / 2*int_a
-            x2 = (-int_b - descr ** (1/2)) / 2*int_a
-            descr_message = 'Квадратное уравнение имеет два действительных корня:'
-        elif descr == 0:
-            x1 = x2 = (-b) / (2*a)
-            descr_message = 'Дискриминант равен нулю, квадратное уравнение имеет один действительный корень:'
-        else:
-            descr_message = 'Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений.'
-    return (request, 'results.html', message_int_a, message_int_b, message_int_c, descr, x1, x2)
+    if a != '0':
+        if a.isdigit() and b.isdigit() and c.isdigit():
+            if a != '' and b != '' and c != '':
+                descr = int(b) ** 2 - 4 * int(a) * int(c)
+                if descr > 0:
+                    x1 = (-int(b) + descr ** (1/2)) / 2*int(a)
+                    x2 = (-int(b) - descr ** (1/2)) / 2*int(a)
+                elif descr == 0:
+                    x1 = x2 = (-b) / (2*a)
+    return render(request, 'quadratic/results.html', {'descr':descr, 'x1':x1, 'x2':x2, 'a':a, 'b':b, 'c':c})
