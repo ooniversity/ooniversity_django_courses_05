df917b329bd4ea3f7c2c1281aa3a3729311de9c9
Sergiy Persianov <sergiy.persianov@gmial.com>
2016-11-16 19:38:59 +0200

    K_Z_4_first_run

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'quadratic.apps.QuadraticConfig',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [BASE_DIR],
-        'APP_DIRS': False,
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
