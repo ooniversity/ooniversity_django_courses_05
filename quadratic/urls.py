1f5b219845f66437e1ab6c76d02e9b389fa42f83
vento357 <vento357@gmail.com>
2016-11-17 23:59:19 +0200

    w

--- quadratic/urls.py
+++ quadratic/urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls import url
-from . import views
+from django.conf.urls import patterns, url
+
+from quadratic import views
 
 
-app_name = 'quadratic'
-urlpatterns = [
-url(r'^results/$', views.quadratic_results, name='results'),
-]
+urlpatterns = patterns('',
+    #url(r'^results/$', views.quadratic_results, name='results'),
+	#url(r'^$', views.IndexView.as_view(), name='index'),
+	url(r'^results/$', views.QuadraticResultsView.as_view(), name='results'),
+)
