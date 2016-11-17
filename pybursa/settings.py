62604ee710345474b16be5a0ffa36a0cea8319cf
sergeychepurnykh <sergeychepurnykh@gmail.com>
2016-11-17 20:13:31 +0800

    KR3_1

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -119,3 +119,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+TEMPLATES = [{'APP_DIRS': True,
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, "templates"),],
+        'OPTIONS': {'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',],},},]
