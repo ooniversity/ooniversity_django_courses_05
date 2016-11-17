29249ad3aae46c92bee76545ae81521856546b6c
eugalata <eugalata@ubuntu-16.04>
2016-11-17 21:17:34 +0200

    v23

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -58,7 +58,7 @@ TEMPLATES = [
         'DIRS': 
         [
             'templates',
-            'quadratic/templates'
+            'quadratic/templates/quadratic'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
