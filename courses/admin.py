509c404b3aa963a7bcc28af26bd6b12ec2daa12b
brown_fox <m_sunny@list.ru>
2016-11-24 19:54:24 +0200

    Customize CoursesAdmin

--- courses/admin.py
+++ courses/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from .models import Course, Lesson
+from django.db import models
+from django.forms import widgets
 
-admin.site.register(Course)
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    fields = [('subject', 'description', 'order')]
+
+class CourseAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['name', 'short_description']
+    fields = ['name', 'short_description', 'description']
+    inlines = [LessonInline]
+
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson)
 
 # Register your models here.
