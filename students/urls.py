5a514c46b04db4fa6945f9a32347ade3d7fcd356
Lyudmyla Kaluzhynova <kaluzhynova@gmail.com>
2016-11-26 11:47:45 +0200

    KR 5.1

---
+++ students/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+from .views import list_view
+
+app_name = 'students'
+urlpatterns = [
+url(r'^$', views.list_view, name='list_view'),
+url(r'^(?P<student_id>[0-9]+)$', views.detail, name='detail'),
+]
