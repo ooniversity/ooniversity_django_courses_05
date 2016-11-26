2c5e32a9012124913f6a7562e9014143fe989d56
Lyudmyla Kaluzhynova <kaluzhynova@gmail.com>
2016-11-26 13:15:00 +0200

    KR6 __str__

--- coaches/models.py
+++ coaches/models.py
@@ -6,14 +6,14 @@ from django.contrib.auth.models import User
 class Coach(models.Model):
     user = models.OneToOneField(User,)
     date_of_birth = models.DateField()
-    gender = models.CharField(max_length=1, choices = (('m', 'Male'), ('f', 'Female')))
-    phone = models.CharField(max_length=15)    # телефон
-    address = models.CharField(max_length=255)        # адрес
-    skype = models.CharField(max_length=25)
+    gender = models.CharField(max_length = 1, choices = (('m', 'Male'), ('f', 'Female')))
+    phone = models.CharField(max_length = 15)    # телефон
+    address = models.CharField(max_length = 255)        # адрес
+    skype = models.CharField(max_length = 25)
     description = models.TextField()         
     
     def __str__(self):
-        return self.user.first_name
+        return self.user.username
     
 
 
