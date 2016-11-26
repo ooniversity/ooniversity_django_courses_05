820996edd6d9aa1ea6bed978e6cb9085f795ec17
TatyanaPaschenko <vivid.tanya@gmail.com>
2016-11-27 00:30:30 +0200

    KR 5.1 02

--- students/urls.py
+++ students/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'students'
 urlpatterns = [
-    url(r'^$', views.list_view , name='list_view '),
+    url(r'^$', views.list_view , name='list_view'),
     url(r'^(?P<pk>[0-9]+)/$', views.detail, name='detail'),
 
 ]
