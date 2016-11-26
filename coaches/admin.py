1ba4c08a95bed01d695d894d94249e7a10efcc32
sergeychepurnykh <sergeychepurnykh@gmail.com>
2016-11-27 07:40:09 +0800

    KR6_0

---
+++ coaches/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Coach
+
+# Register your models here.
+
+class CoachAdmin(admin.ModelAdmin):
+    list_display = ['get_first_name', 'get_last_name', 'gender', 'skype', 'description']
+    list_filter = ['user__is_staff', ]
+
+    def get_first_name(self, obj):
+        return obj.user.first_name
+
+    def get_last_name(self, obj):
+        return obj.user.last_name
+
+
+admin.site.register(Coach, CoachAdmin)
---
+++ coaches/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^(?P<coach_id>[0-9]+)/$', views.detail, name='detail'),
+]
