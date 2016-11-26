0738544d196e16a906cd99b4856727cc6d158d1d
toldrew <tol_drew@mail.ru>
2016-11-27 01:08:03 +0700

    KZ_5.2

--- students/admin.py
+++ students/admin.py
@@ -1,4 +1,24 @@
 from django.contrib import admin
 from students.models import Student
 
-admin.site.register(Student)
+
+
+class StudentAdmin (admin.ModelAdmin):
+    list_display = ('full_name', 'email', 'skype')
+    search_fields = ('surname', 'email',)
+    list_filter = ('courses',)
+    filter_horizontal = ('courses',)
+    #inlines = (LessonInline,)
+    fieldsets = (
+        ('Personal info', {
+            'fields': ('name', 'surname', 'date_of_birth')
+        }),
+        ('Contact info', {
+            'fields': ('email', 'phone', 'address', 'skype')
+        }),
+        (None, {
+            'fields': ('courses',)
+        })
+    )
+
+admin.site.register(Student, StudentAdmin)
