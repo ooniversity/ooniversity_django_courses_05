35d41a19347b4c17aa22e9dceccbbbca592caa5d
a-n-t <freebsd.in@gmail.com>
2016-11-26 14:50:11 -0500

    kr5_02v2

--- courses/admin.py
+++ courses/admin.py
@@ -3,9 +3,6 @@ from .models import Course, Lesson
 
 # Register your models here.
 
-admin.site.register(Course)
-admin.site.register(Lesson)
-
 class LessonInline(admin.TabularInline):
     model = Lesson
     extra = 0
@@ -14,3 +11,6 @@ class CourseAdmin(admin.ModelAdmin):
     list_display = ('name', 'short_description')
     search_fields = ['name']
     inlines = [LessonInline]
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Lesson)
