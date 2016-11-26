cfb2c2758335181ad763309207bdf5634f6ee960
Your Name <you@example.com>
2016-11-26 22:17:35 +0200

    new5_1

---
+++ courses/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+# from coaches.models import Coach
+
+class Course(models.Model):
+    """
+    -  name                         (CharField)     # название
+    -  short_description            (CharField)     # краткое описание
+    -  description                  (TextField)     # полное описание
+    """
+    name = models.CharField(max_length=60)
+    short_description = models.CharField(max_length=255)
+    description = models.TextField(default=None)
+    #coach = models.ForeignKey(Coach, null=True, blank=True, related_name='coach_courses')
+    #assistant = models.ForeignKey(Coach, null=True, blank=True, related_name='assistant_courses')
+
+
+    def __str__(self):
+        return self.name
+
+class Lesson(models.Model):
+    """
+    -  subject                    (CharField)                          # тема
+    -  description                (TextField)                          # описание
+    -  course                     (ForeignKey на Course)               # курс
+    -  order                      (PositiveIntegerField)               # номер по порядку
+    """
+    subject = models.CharField(max_length=60)
+    description = models.TextField(max_length=255)
+    course = models.ForeignKey(Course)
+    order = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.subject
+
+    def __repr__(self):
+        return self.subject
---
+++ students/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from courses.models import Course
+
+class Student(models.Model):
+    """
+    -  name                         (CharField)                                     # имя
+    -  surname                      (CharField)                                     # фамилия
+    -  date_of_birth                (DateField)                                     # датa рождения
+    -  email                        (EmailField)
+    -  phone                        (CharField)                                     # телефон
+    -  address                      (CharField)                                     # адрес
+    -  skype                        (CharField)
+    -  courses                      (ManyToManyField на Course)    # курсы, на которых учится студент
+    """
+    name = models.CharField(verbose_name='Student name', max_length=60)
+    surname = models.CharField(verbose_name='Student surname', max_length=60)
+    date_of_birth = models.DateField()
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=16, null=True, blank=True)
+    address = models.CharField(max_length=255)
+    skype = models.CharField(max_length=15, null=True, blank=True)
+    courses = models.ManyToManyField(Course)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def full_name(self):
+        return '%s %s' % (self.name, self.surname)
