935e1be667d5bfd1e6bcd5834689741d9da23174
TatyanaPaschenko <vivid.tanya@gmail.com>
2016-11-27 23:09:54 +0200

    KR 6 02

--- courses/views.py
+++ courses/views.py
@@ -6,6 +6,7 @@ from coaches.models import Coach
 def detail(request, pk):
     courses = Course.objects.filter(id=pk)
     course = courses[0]
-    coaches = Coach.objects.filter(id=pk)
+    coaches = Coach.objects.filter(coach_courses=pk)
+    assistants = Coach.objects.filter(assistant_courses=pk)
     lessons = Lesson.objects.filter(course=pk)
-    return render(request, 'courses/detail.html', {'course': course, 'lessons': lessons, 'pk': pk, 'coaches': coaches})
+    return render(request, 'courses/detail.html', {'course': course, 'lessons': lessons, 'pk': pk, 'coaches': coaches, 'assistants': assistants})
