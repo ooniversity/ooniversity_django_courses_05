749b970644008ed298cfdba6e540e8958d4a8378
Lyudmyla Kaluzhynova <kaluzhynova@gmail.com>
2016-12-03 01:10:56 +0200

    KR_8 -index

--- courses/urls.py
+++ courses/urls.py
@@ -4,9 +4,9 @@ from .views import detail, add, edit, remove, add_lesson
 
 app_name = 'courses'
 urlpatterns = [
-url(r'^(?P<course_id>[0-9]+)/$', views.detail, name='detail'),
+url(r'^(?P<course_id>\d+)/$', views.detail, name='detail'),
 url(r'^add/$', add, name='add'),
-url(r'^edit/(?P<course_id>[0-9]+)/$', edit, name='edit'),
-url(r'^remove/(?P<course_id>[0-9]+)/$', remove, name='remove'),
-url(r'^(?P<course_id>[0-9]+)/add_lesson$', add_lesson, name='add_lesson'),
+url(r'^edit/(?P<course_id>\d+)/$', edit, name='edit'),
+url(r'^remove/(?P<course_id>\d+)/$', remove, name='remove'),
+url(r'^(?P<course_id>\d+)/add_lesson$', add_lesson, name='add_lesson'),
 ]
