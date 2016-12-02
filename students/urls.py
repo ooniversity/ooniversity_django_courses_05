5721151ad350c49c509821def4850ef8176d832d
kazantip <kazzantipoff@gmail.com>
2016-12-02 13:37:47 +0200

    Added and configured add/edit/remove pages at 0.12.2016_13.37

--- students/urls.py
+++ students/urls.py
@@ -2,7 +2,11 @@ from django.conf.urls import url
 from . import views
 
 app_name = 'students'
+
 urlpatterns = [
     url(r'^$', views.list_view, name='list_view'),
-    url(r'^(?P<stud_id>[\d]+)/$', views.detail, name='detail')
+    url(r'^(?P<stud_id>[\d]+)/$', views.detail, name='detail'),
+    url(r'^add/$', views.add, name='add'),
+    url(r'^edit/(?P<student_id>\d+)/$', views.edit, name='edit'),
+    url(r'^remove/(?P<student_id>\d+)/$', views.remove, name='remove')
 ]
