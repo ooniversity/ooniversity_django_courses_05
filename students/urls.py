fafad78695f197c2855717c650a2b5ad537c21e7
ooniversity <ooniversity@raccoongang.com>
2016-12-02 16:52:15 +0200

    KR 8 opersianova

--- students/urls.py
+++ students/urls.py
@@ -2,8 +2,12 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'students'
 
 urlpatterns = [
     url(r'^$', views.list_view, name='list_view'),
-    url(r'^(?P<pk>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>\d+)/', views.detail, name='detail'),
+    url(r'^add/', views.create, name='add'),
+    url(r'^edit/(?P<pk>\d+)/', views.edit, name='edit'),
+    url(r'^remove/(?P<pk>\d+)/', views.remove, name='remove'),
 ]
