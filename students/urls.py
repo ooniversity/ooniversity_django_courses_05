236b79e1d057b6d129fe95b84d8e45133c9950f4
Sergiy Persianov <sergiy.persianov@gmial.com>
2016-12-03 23:21:40 +0200

    kz_7_3

--- students/urls.py
+++ students/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
-
 from . import views
 
 
 urlpatterns = [
+    url(r'^remove/(?P<pk>\d+)/', views.remove, name='remove'),
+    url(r'^edit/(?P<pk>\d+)/', views.edit, name='edit'),
+    url(r'^add/', views.create, name='add'),
     url(r'^$', views.list_view, name='list_view'),
     url(r'^(?P<pk>\d+)/$', views.detail, name='detail'),
 ]
