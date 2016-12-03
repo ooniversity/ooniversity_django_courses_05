fef11ba7fa638011f907eeb734415d4eeea24e4c
nikkolay <nick.klimenuk@gmail.com>
2016-12-03 19:13:37 +0200

    done

--- quadratic/views.py
+++ quadratic/views.py
@@ -1,21 +1,32 @@
 from django.shortcuts import render
-from .form import QuadraticForm
-from .quadratic import discriminant
+from .forms import QuadraticForm
 
 def quadratic_results(request):
-    form = QuadraticForm()
-
-    context = {}
     if len(request.GET) > 0:
         form = QuadraticForm(request.GET)
+        context = {
+            "diskr": "",
+            "form": form
+        }
         if form.is_valid():
-            dict_discriminant = discriminant(form.cleaned_data)
-            context.update(dict_discriminant)
-
-    context['form'] = form
-
+            a = form.cleaned_data['a']
+            b = form.cleaned_data['b']
+            c = form.cleaned_data['c']
+            diskr = b * b - 4 * a * c
+            context['diskr'] = "Дискриминант: {0}".format(diskr)
+            if diskr < 0:
+                context["info_text"] = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
+            elif diskr == 0:
+                x1 = -b / 2 * a
+                context[
+                    "info_text"] = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = {0}".format(
+                    x1)
+            else:
+                x1 = (-b + diskr ** (1 / 2)) / 2 * a
+                x2 = (-b - diskr ** (1 / 2)) / 2 * a
+                context["info_text"] = "Квадратное уравнение имеет два действительных корня: x1 = {0}, x2 = {1}".format(
+                    x1, x2)
+    else:
+        form = QuadraticForm()
+        context = {'form': form}
     return render(request, 'quadratic/results.html', context)
-
-#"This equation has one solutions: "
-#"This equation has two solutions: ", x1, " or", x2
-#"This equation has no real solution"
