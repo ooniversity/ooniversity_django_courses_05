fdbfe0255e8bd02b3015921762f4db173233755b
oleg-tim <book1978@gmail.com>
2016-12-03 22:44:32 +0200

    8_1

--- courses/urls.py
+++ courses/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^(?P<course_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^add/$', views.add, name='add'),
+    url(r'^edit/(?P<course_id>[0-9]+)/$', views.edit, name='edit'),
+    url(r'^remove/(?P<course_id>[0-9]+)/$', views.remove, name='remove'),
+    url(r'^(?P<course_id>[0-9]+)/add_lesson/$', views.add_lesson, name='add_lesson'),
 ]
