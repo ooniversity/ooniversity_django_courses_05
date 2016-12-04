2a337a9e05c2bcec793e9357caec033a8147f800
toldrew <tol_drew@mail.ru>
2016-12-04 21:01:42 +0700

    fix_bug_7_2_2

--- quadratic/urls.py
+++ quadratic/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from .views import quadratic_results
 
+app_name = 'quadratic'
 urlpatterns = [
-    url(r'^results/$', quadratic_results),
-
+    url(r'^results/$', quadratic_results, name="quadratic_results"),
 ]
--- quadratic/views.py
+++ quadratic/views.py
@@ -1,18 +1,5 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django import forms
-
-
-class QuadraticForm(forms.Form):
-    a = forms.IntegerField(label='коэффициент a')
-    b = forms.IntegerField(label='коэффициент b')
-    c = forms.IntegerField(label='коэффициент c')
-
-    def clean_a(self):
-        validation_a = self.cleaned_data['a']
-        if validation_a == 0:
-            raise forms.ValidationError("коэффициент при первом слагаемом уравнения не может быть равным нулю")
-        return validation_a
+from django.shortcuts import render
+from .forms import QuadraticForm
 
 
 def quadratic_results(request):
