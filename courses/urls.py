12468c98ef3e50d03c0003628f9146a9253bcea2
toldrew <tol_drew@mail.ru>
2016-12-04 23:02:56 +0700

    fix_bug_KZ_8_test_4_and_5

--- courses/urls.py
+++ courses/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^add/$', add, name='add'),
     url(r'^edit/(?P<pk>[0-9]+)/$', edit, name='edit'),
     url(r'^remove/(?P<pk>[0-9]+)/$', remove, name='remove'),
-    url(r'^(?P<pk>[0-9]+)/add-lesson/$', add_lesson, name='add-lesson'),
+    url(r'^(?P<pk>[0-9]+)/add-lesson$', add_lesson, name='add-lesson'),
 ]
