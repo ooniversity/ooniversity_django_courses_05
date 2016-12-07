e687b2541b867fbb68f421f87d3cbc38649796ef
Lyudmyla Kaluzhynova <kaluzhynova@gmail.com>
2016-12-07 13:54:53 +0200

    KR 9.1

--- students/urls.py
+++ students/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import url
 from . import views
-from .views import list_view, add, edit, remove
 
 app_name = 'students'
 urlpatterns = [
-url(r'^$', views.list_view, name='list_view'),
-url(r'^(?P<student_id>[0-9]+)/$', views.detail, name='detail'),
-url(r'^add/$', add, name='add'),
-url(r'^edit/(?P<student_id>[0-9]+)/$', edit, name='edit'),
-url(r'^remove/(?P<student_id>[0-9]+)/$', remove, name='remove'),
+url(r'^$', views.StudentListView.as_view(), name='list'),
+url(r'^(?P<pk>\d+)/$', views.StudentDetailView.as_view(), name='detail'),
+url(r'^add/$', views.StudentCreateView.as_view(), name='add'),
+url(r'^edit/(?P<pk>\d+)/$', views.StudentUpdateView.as_view(), name='edit'),
+url(r'^remove/(?P<pk>\d+)/$', views.StudentDeleteView.as_view(), name='remove'),
 ]
