9bf9c29c662b88d5f93d3012c401bc9c0519bc06
Lyudmyla Kaluzhynova <kaluzhynova@gmail.com>
2016-12-08 22:38:08 +0200

    KR 10-1 modelform

--- feedbacks/forms.py
+++ feedbacks/forms.py
@@ -1,7 +1,7 @@
 from feedbacks.models import Feedback
 from django import forms
 
-class FeedbackModelForm(forms.ModelForm):
+class FeedbackForm(forms.ModelForm):
     class Meta:
         model = Feedback
         exclude = []
