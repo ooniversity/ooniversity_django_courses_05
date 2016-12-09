759d0962f01ff48f7aea62b9f1b801f7f9da36a0
Anton Kramarov <kas_2002@mail.ru>
2016-12-09 02:20:35 +0200

    KZ10.1 - 4

--- feedbacks/views.py
+++ feedbacks/views.py
@@ -1,10 +1,11 @@
+from django.conf import settings
 from django.shortcuts import redirect
 from django.urls import reverse, reverse_lazy
 from feedbacks.forms import FeedbackForm
 from feedbacks.models import Feedback
 from django.views.generic.edit import CreateView
 from django.contrib import messages
-from django.core.mail import mail_admins
+from django.core.mail import mail_admins, send_mail
 
 
 class FeedbackView(CreateView):
@@ -15,7 +16,8 @@ class FeedbackView(CreateView):
 
     def form_valid(self, form):
         response = super().form_valid(form)
-        mail_admins(self.object.subject, self.object.message, fail_silently=False, connection=None, html_message=None)
+        #mail_admins(self.object.subject, self.object.message, fail_silently=False, connection=None, html_message=None)
+        send_mail(self.object.subject, self.object.message, self.object.from_email, settings.ADMINS)
         message = "Thank you for your feedback! We will keep in touch with you very soon!"
         messages.success(self.request, message)
         return response
