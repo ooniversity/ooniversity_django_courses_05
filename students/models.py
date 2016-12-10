78b8820f98e2647f59d8651f29cb3afa93f962a7
Lyudmyla Kaluzhynova <kaluzhynova@gmail.com>
2016-12-11 00:10:47 +0200

    KR10_2 student_model

--- students/models.py
+++ students/models.py
@@ -6,7 +6,7 @@ class Student(models.Model):
     name=models.CharField(max_length=60)         
     surname=models.CharField(max_length=60)     
     date_of_birth=models.DateField()    
-    email=models.EmailField(unique=True)
+    email=models.EmailField()
     phone=models.CharField(max_length=15)   
     address=models.CharField(max_length=255)      
     skype=models.CharField(max_length=25)
