263a65dbdb3f0f83113e140709889299a670a9a2
sergeychepurnykh <sergeychepurnykh@gmail.com>
2016-12-11 06:56:36 +0800

    KR10_2_4

--- courses/urls.py
+++ courses/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 app_name = 'courses'
+
 urlpatterns = [
     url(r'^(?P<pk>\d+)/$', views.CourseDetailView.as_view(), name='detail'),
     url(r'^add/$', views.CourseCreateView.as_view(), name='add'),
--- students/urls.py
+++ students/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 app_name = 'students'
+
 urlpatterns = [
     url(r'^$', views.StudentListView.as_view(), name='list_view'),
     url(r'^(?P<pk>[\d]+)/$', views.StudentDetailView.as_view(), name='detail'),
