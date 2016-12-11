0afa55028deae9f268b57c3ca3495dd1b0c90bdd
toldrew <tol_drew@mail.ru>
2016-12-11 17:08:45 +0700

    KZ_10_1_fix_bug_1

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,22 +1,7 @@
-"""pybursa URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-
 from django.conf.urls import include, url
 from django.contrib import admin
 from pybursa.views import index, contact, student_detail
+from feedbacks import views
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
@@ -28,5 +13,5 @@ urlpatterns = [
     url(r'^students/', include('students.urls')),
     url(r'^coaches/', include('coaches.urls')),
     url(r'^student_detail/$', student_detail, name='student_detail'),
-    url(r'^feedback/', include('feedbacks.urls')),
+    url(r'^feedback/', views.FeedbackView.as_view(), name='feedback'),
 ]
