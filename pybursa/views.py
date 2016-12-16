347cd96f18d205fd1ace2e78ba32814989c2aebb
vento357 <vento357@gmail.com>
2016-12-17 00:33:27 +0300

    Add files via upload

--- pybursa/views.py
+++ pybursa/views.py
@@ -1,20 +1,11 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.urls import reverse
-from django.views import generic
+from courses.models import Course
 
 
-class IndexView(generic.ListView):
-    template_name = 'index.html'
-    context_object_name = 'latest_question_list'
-    def index(request):
-        return render(request, "index.html")
+def index(request):
+    courses = Course.objects.all()
+    return render(request, 'index.html', {'courses': courses})
+
 
 def contact(request):
-    return render(request, "contact.html")
-
-def student_list(request):
-    return render(request, "student_list.html")
-
-def student_detail(request):
-    return render(request, "student_detail.html")
+    return render(request, 'contact.html')
