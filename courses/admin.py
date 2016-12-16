717674e4d0d39f7b92fcf225da7aae677581fa1f
vento357 <vento357@gmail.com>
2016-12-17 01:04:43 +0300

    Add files via upload

--- courses/admin.py
+++ courses/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from .models import Course, Lesson
+from courses.models import Course, Lesson
 
-# Register your models here.
 
-admin.site.register(Course)
+class LessonInline(admin.TabularInline):
+    model = Lesson
+    extra = 0
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'short_description',)
+    search_fields = ['name']
+
+    inlines = [LessonInline]
+
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson)
+admin.site.site_header = 'PyBursa Administration'
 
