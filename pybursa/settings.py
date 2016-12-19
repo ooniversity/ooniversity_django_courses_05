7ea09129e701d590451eb410a882b3eaf504c417
toldrew <tol_drew@mail.ru>
2016-12-19 13:50:44 +0700

    KZ_12_1

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'mwyta+5k#p3un@-od1d^#lmm^e+_k3g1)&c+vn2d_@utsvzdc#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
