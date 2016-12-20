7639348bccbc52cb0911b4d2dcced2f161a7346b
vento357 <vento357@gmail.com>
2016-12-21 02:27:19 +0300

    Add files via upload

--- pybursa/views.py
+++ pybursa/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, render_to_response
+from django.template import RequestContext
+
 from courses.models import Course
 
 
@@ -9,3 +11,17 @@ def index(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+
+# def handler404(request):
+#     response = render_to_response('404.html', { 'message' : 'Sorry, page is not found' },
+#                                   context_instance=RequestContext(request))
+#     response.status_code = 404
+#     return response
+
+
+# def handler500(request):
+#     response = render_to_response('500.html', { 'message' : 'Sorry, internal server error occurred' },
+#                                   context_instance=RequestContext(request))
+#     response.status_code = 500
+#     return response
