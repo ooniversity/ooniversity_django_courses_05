63156584a217021e3599edffda4a8a97d4f47c93
Sergiy Persianov <sergiy.persianov@gmial.com>
2016-12-21 17:41:29 +0200

    kz12_1 first run

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%zf)ehhi=@m*nx959*c$@678&++_dpsa)^6$lwh!t&3))tew*j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
